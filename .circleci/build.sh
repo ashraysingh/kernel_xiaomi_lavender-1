463c08250aca6fd1baa49f06abd03804e2e5a144
ehtesham01 <ehteshammalik4@gmail.com>
2021-06-21 15:21:29 +0530

    Updated build.sh for extra NewCam build

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -6,9 +6,7 @@ git clone https://github.com/sohamxda7/llvm-stable -b gcc32  --depth=1 gcc32
 git clone --depth=1 https://github.com/who-em-i/AnyKernel3 AnyKernel
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-CAMERA=oldCam
 TANGGAL=$(date +"%F-%S")
-START=$(date +"%s")
 KERNEL_DIR=$(pwd)
 PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
 export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
@@ -32,6 +30,7 @@ function push() {
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
         -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi Note 7/7s (lavender)</b> | <b>$KBUILD_COMPILER_STRING</b>"
+    rm *.zip *.gz-dtb
 }
 # Fin Error
 function finerr() {
@@ -58,6 +57,23 @@ function compile() {
     fi
     cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
+function compile2() {
+    echo "CONFIG_XIAOMI_NEWCAM=y" >> arch/arm64/configs/lavender-perf_defconfig
+    make clean -j$(nproc --all) && rm -r out
+    make -s -j$(nproc --all) O=out ARCH=arm64 lavender-perf_defconfig
+    make -j$(nproc --all) O=out \
+                          ARCH=arm64 \
+                          CC=clang \
+                          CLANG_TRIPLE=aarch64-linux-gnu- \
+                          CROSS_COMPILE=aarch64-linux-android- \
+                          CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+    if ! [ -a "$IMAGE" ]; then
+        finerr
+        exit 1
+    fi
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+}
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
@@ -65,8 +81,17 @@ function zipping() {
     cd ..
 }
 sendinfo
+CAMERA=OldCam
+START=$(date +"%s")
 compile
-zipping
 END=$(date +"%s")
 DIFF=$(($END - $START))
+zipping
+push
+CAMERA=NewCam
+START=$(date +"%s")
+compile2
+END=$(date +"%s")
+DIFF=$(($END - $START))
+zipping
 push
